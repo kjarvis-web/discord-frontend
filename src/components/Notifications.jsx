84d4e67afa35a2b235f6c63c90fafe909ad612a0
norristown <jarvasity@gmail.com>
2024-05-30 16:53:41 -0400

    styled notification icon

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,5 +1,6 @@
 import { HiUser } from 'react-icons/hi2';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Notifications = () => {
   const user = useSelector((state) => state.users.loggedUser);
@@ -8,15 +9,17 @@ const Notifications = () => {
 
   console.log(user);
   return (
-    <div className="flex items-center">
-      <button>
-        <HiUser className="w-5 h-5" />
-        {friendRequests.length !== 0 && (
-          <span className="rounded-full bg-red-500 h-5 w-5 font-semibold text-xs flex items-center justify-center">
-            {friendRequests.length}
-          </span>
-        )}
-      </button>
+    <div>
+      <Link to="/">
+        <button className="flex relative">
+          <HiUser className="w-7 h-7 hover:text-blue-600" />
+          {friendRequests.length !== 0 && (
+            <span className="rounded-full bg-red-500 h-4 w-4 font-semibold text-xs flex items-center justify-center border border-red-500 absolute -right-1 -top-2">
+              {friendRequests.length}
+            </span>
+          )}
+        </button>
+      </Link>
     </div>
   );
 };
